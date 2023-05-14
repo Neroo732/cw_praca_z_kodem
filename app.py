8dbef96cdbedb2c66051b76dd8948223b99628cf
Neroo732 <londas12@gmail.com>
2023-05-14 16:13:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
+"""
+    Dodaje dwie liczby.
+
+    :param a: Pierwsza liczba.
+    :param b: Druga liczba.
+    :return: Suma dwóch
+ """      
+        
 @app.route('/')
 def index():
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+"""
+    Dodaje dwie liczby.
 
-
+    :param a: Pierwsza liczba.
+    :param b: Druga liczba.
+    :return: Suma dwóch
+"""
 if __name__ == "__main__":
     app.run(debug=True)

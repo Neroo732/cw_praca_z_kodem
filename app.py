2dd24f629e4739ba6d9bf642b5af36ba1e6fb5c8
Neroo732 <londas12@gmail.com>
2023-05-14 16:21:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,13 @@ Ten moduł zawiera funkcje do ...
 from flask import Flask
 
 app = Flask(__name__)
-"""
-Opi
-
-Ten moduł zawiera funkcje do ...
-"""
 
 
 @app.route('/')
 def index():
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+
 """
 Opi
 

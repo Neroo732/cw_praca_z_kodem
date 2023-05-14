8137e27c37d22c2797348c26ad05956d4ea6f895
Neroo732 <londas12@gmail.com>
2023-05-14 16:14:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,14 @@ Ten moduł zawiera funkcje do ...
 from flask import Flask
 
 app = Flask(__name__)
-
-
 """
-    Dodaje dwie liczby.
+Opi
 
-    :param a: Pierwsza liczba.
-    :param b: Druga liczba.
-    :return: Suma dwóch
- """      
-        
+Ten moduł zawiera funkcje do ...
+"""    
 @app.route('/')
 def index():
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
-"""
-    Dodaje dwie liczby.
 
-    :param a: Pierwsza liczba.
-    :param b: Druga liczba.
-    :return: Suma dwóch
-"""
 if __name__ == "__main__":
     app.run(debug=True)
